cab9607e310ceef6d0b18b838e42bed9009eb9e3
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-09-01 23:31:48 +0800

    add deployment

--- packages/hardhat/deploy/deploy-01-CoffeeChat.ts
+++ packages/hardhat/deploy/deploy-01-CoffeeChat.ts
@@ -2,41 +2,40 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers, upgrades, network } from "hardhat";
 
-const initBaseURI = "https://testnet.tableland.network/query?";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { save } = hre.deployments;
     const chainId = await hre.getChainId();
     console.log("network:", network.name, `(${chainId})`);
 
-    const Tind3rMembership = await ethers.getContractFactory("Tind3rMembership");
+    const CoffeeChat = await ethers.getContractFactory("CoffeeChat");
 
     // deploy
     const proxy = await upgrades.deployProxy(
-        Tind3rMembership,
-        [initBaseURI],
+        CoffeeChat,
+        [],
         { kind: 'uups' }
     );
     await proxy.deployed();
-    console.log("Tind3rMembership Proxy:", proxy.address);
+    console.log("CoffeeChat Proxy:", proxy.address);
     const logicAddress = await upgrades.erc1967.getImplementationAddress(proxy.address);
-    console.log("Tind3rMembership Logic:", logicAddress);
+    console.log("CoffeeChat Logic:", logicAddress);
 
     // save proxy artifact
-    const proxyArtifact = await hre.deployments.getArtifact('Tind3rMembership');
+    const proxyArtifact = await hre.deployments.getArtifact('CoffeeChat');
     const proxyDeployments = {
         address: proxy.address,
         ...proxyArtifact
     };
-    await save('Tind3rMembership', proxyDeployments);
+    await save('CoffeeChat', proxyDeployments);
 
     // save logic artifact for verification
-    const logicArtifact = await hre.deployments.getExtendedArtifact('Tind3rMembership');
+    const logicArtifact = await hre.deployments.getExtendedArtifact('CoffeeChat');
     const logicDeployments = {
         address: logicAddress,
         ...logicArtifact
     };
-    await save('Tind3rMembershipLogic', logicDeployments);
+    await save('CoffeeChat', logicDeployments);
 };
 export default func;
-func.tags = ["deployMembership", "deploy"];
+func.tags = ["deployCoffeeChat", "deploy"];
