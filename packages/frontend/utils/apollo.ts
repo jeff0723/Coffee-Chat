5bc808c0d0f54e3ac8e1764c9103d6167df1bcbf
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-09-02 23:00:31 +0800

    change url

--- packages/frontend/utils/apollo.ts
+++ packages/frontend/utils/apollo.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-const APIURL = 'https://api.studio.thegraph.com/query/coffeechat/'
+const APIURL = 'https://api.studio.thegraph.com/query/19892/coffeechat/v0.0.3'
 
 export const client = new ApolloClient({
     uri: APIURL,
