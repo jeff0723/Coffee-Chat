3edf5480bfde65b56e1f032df3e6025f4d2abd43
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-09-02 21:21:23 +0800

    add subgraph

---
+++ packages/subgraph/tests/coffee-chat-utils.ts
@@ -0,0 +1,239 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  AdminChanged,
+  Approval,
+  ApprovalForAll,
+  BeaconUpgraded,
+  CoffeChatIntialize,
+  ConsecutiveTransfer,
+  Initialized,
+  OwnershipTransferred,
+  Transfer,
+  Upgraded
+} from "../generated/CoffeeChat/CoffeeChat"
+
+export function createAdminChangedEvent(
+  previousAdmin: Address,
+  newAdmin: Address
+): AdminChanged {
+  let adminChangedEvent = changetype<AdminChanged>(newMockEvent())
+
+  adminChangedEvent.parameters = new Array()
+
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdmin",
+      ethereum.Value.fromAddress(previousAdmin)
+    )
+  )
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam("newAdmin", ethereum.Value.fromAddress(newAdmin))
+  )
+
+  return adminChangedEvent
+}
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createBeaconUpgradedEvent(beacon: Address): BeaconUpgraded {
+  let beaconUpgradedEvent = changetype<BeaconUpgraded>(newMockEvent())
+
+  beaconUpgradedEvent.parameters = new Array()
+
+  beaconUpgradedEvent.parameters.push(
+    new ethereum.EventParam("beacon", ethereum.Value.fromAddress(beacon))
+  )
+
+  return beaconUpgradedEvent
+}
+
+export function createCoffeChatIntializeEvent(
+  placeId: string,
+  lantitude: BigInt,
+  longtitude: BigInt,
+  tokenId: BigInt
+): CoffeChatIntialize {
+  let coffeChatIntializeEvent = changetype<CoffeChatIntialize>(newMockEvent())
+
+  coffeChatIntializeEvent.parameters = new Array()
+
+  coffeChatIntializeEvent.parameters.push(
+    new ethereum.EventParam("placeId", ethereum.Value.fromString(placeId))
+  )
+  coffeChatIntializeEvent.parameters.push(
+    new ethereum.EventParam(
+      "lantitude",
+      ethereum.Value.fromUnsignedBigInt(lantitude)
+    )
+  )
+  coffeChatIntializeEvent.parameters.push(
+    new ethereum.EventParam(
+      "longtitude",
+      ethereum.Value.fromUnsignedBigInt(longtitude)
+    )
+  )
+  coffeChatIntializeEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return coffeChatIntializeEvent
+}
+
+export function createConsecutiveTransferEvent(
+  fromTokenId: BigInt,
+  toTokenId: BigInt,
+  from: Address,
+  to: Address
+): ConsecutiveTransfer {
+  let consecutiveTransferEvent = changetype<ConsecutiveTransfer>(newMockEvent())
+
+  consecutiveTransferEvent.parameters = new Array()
+
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam(
+      "fromTokenId",
+      ethereum.Value.fromUnsignedBigInt(fromTokenId)
+    )
+  )
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam(
+      "toTokenId",
+      ethereum.Value.fromUnsignedBigInt(toTokenId)
+    )
+  )
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+
+  return consecutiveTransferEvent
+}
+
+export function createInitializedEvent(version: i32): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(version))
+    )
+  )
+
+  return initializedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
+
+export function createUpgradedEvent(implementation: Address): Upgraded {
+  let upgradedEvent = changetype<Upgraded>(newMockEvent())
+
+  upgradedEvent.parameters = new Array()
+
+  upgradedEvent.parameters.push(
+    new ethereum.EventParam(
+      "implementation",
+      ethereum.Value.fromAddress(implementation)
+    )
+  )
+
+  return upgradedEvent
+}
