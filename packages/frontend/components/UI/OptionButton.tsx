765b6ff0a0e47409253b6e0cec34fed01305f273
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-09-07 18:15:20 +0800

    change clsx

--- packages/frontend/components/UI/OptionButton.tsx
+++ packages/frontend/components/UI/OptionButton.tsx
@@ -7,17 +7,28 @@ import { CoffeeOutlined, CameraOutlined, StarOutlined, CarryOutOutlined } from '
 import { Modal, Tooltip } from 'antd';
 import CoffeeChatList from './CoffeeChatList';
 import ScanQrcodeModal from './ScanQrcodeModal';
-
+import { useMediaQuery } from 'react-responsive'
+import clsx from "clsx"
 
 type Props = {}
 const OptionButton = (props: Props) => {
+    const isMobile = useMediaQuery({
+        query: '(max-width: 475px)'
+    })
     const [optionShow, setOptionShow] = useState(false)
     const [coffeeChatListModalOpen, toggleCoffeeChatList] = useReducer(state => !state, false)
     const [cameraOpen, toggleCamera] = useReducer(state => !state, false)
     const [qrCode, setQrCode] = useState('')
 
     return (
-        <div className="absolute flex flex-col left-5 bottom-5 gap-2">
+        <div className={
+            clsx(
+                {
+                    "left-5 bottom-5": isMobile == false,
+                    "left-5 bottom-10": isMobile == true
+                }
+                , "absolute flex flex-col gap-2")
+        }>
             <Transition appear show={optionShow} as={Fragment}>
                 <Dialog as="div" className="relative z-10" onClose={() => { setOptionShow(false) }}>
                     <Transition.Child
@@ -45,7 +56,12 @@ const OptionButton = (props: Props) => {
                     leave="ease-in duration-200"
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0">
-                    <div className="flex flex-col gap-2 z-20 mb-24">
+                    <div className={
+                        clsx({
+                            "mb-24": isMobile == false,
+                            "mb-28": isMobile == true
+                        }, "flex flex-col gap-2 z-20")
+                    }>
                         <div className="ml-8 flex gap-2  items-center pr-2">
                             <Tooltip title="Open camera">
                                 <div className="w-10 h-10 bg-white rounded-full flex justify-center items-center hover:bg-opacity-90"
@@ -78,7 +94,13 @@ const OptionButton = (props: Props) => {
             </Transition>
 
             <button
-                className='absolute left-5 bottom-5 z-40 rounded-full flex justify-center items-center p-2 bg-white w-16 h-16 hover:text-[#6f4e37] hover:bg-opacity-90'
+                className={
+                    clsx({
+                        "bottom-5": isMobile == false,
+                        "bottom-10": isMobile == true
+                    }, 'absolute left-5  z-40 rounded-full flex justify-center items-center p-2 bg-white w-16 h-16 hover:text-[#6f4e37] hover:bg-opacity-90')
+
+                }
                 onClick={() => setOptionShow(!optionShow)}>
                 <CoffeeOutlined className='text-[25px]' color='#6f4e37' />
             </button>
