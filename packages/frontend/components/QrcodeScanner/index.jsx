b639aa3b9f4a8287a6a1d78c1ce437dcc2b6b6c0
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-09-07 13:45:30 +0800

    use qrcode reader

--- packages/frontend/components/QrcodeScanner/index.jsx
+++ packages/frontend/components/QrcodeScanner/index.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Script from 'next/script';
+import { QrReader } from "react-qr-reader";
 
 
 const Qrscan = () => {
@@ -8,70 +9,83 @@ const Qrscan = () => {
     const [data, setData] = useState("")
     const [qrcode, setQrcode] = useState()
     const [scanning, setScanning] = useState(false)
-    useEffect(() => {
-        if (qrcode)
-            qrcode.callback = (res) => {
-                console.log('hi outside callback')
+    // useEffect(() => {
+    //     if (qrcode) {
+    //         console.log('hi outside callback')
+    //         qrcode.callback = (res) => {
+    //             console.log('hi inside callback')
+    //             if (res) {
+    //                 console.log('hi inside callback')
+    //                 console.log(res)
+    //                 setData(res)
+    //                 setScanning(false)
+    //                 if (videoRef.current) {
+    //                     videoRef.current.srcObject.getTracks().forEach(track => {
+    //                         track.stop();
+    //                     });
+    //                 }
+    //                 canvasRef.current.hidden = true
 
-                if (res) {
-                    console.log('hi inside callback')
 
-                    console.log(res)
-                    setData(res)
-                    setScanning(false)
-                    if (videoRef.current) {
-                        videoRef.current.srcObject.getTracks().forEach(track => {
-                            track.stop();
-                        });
-                    }
-                    canvasRef.current.hidden = true
+    //             }
+    //         };
+    //     }
+    // }, [qrcode])
+    // const tick = () => {
+    //     console.log('tick')
+    //     const canvas = canvasRef.current.getContext("2d");
+    //     canvasRef.current.height = videoRef.current.videoHeight
+    //     canvasRef.current.width = videoRef.current.videoWidth
+    //     canvas.drawImage(videoRef.current, 0, 0, canvasRef.current.height, canvasRef.current.width);
+    //     scanning && requestAnimationFrame(tick);
+    // }
+    // const scan = () => {
+    //     if (qrcode) {
+    //         try {
+    //             qrcode.decode()
+    //         } catch (e) {
+    //             setTimeout(scan, 300);
+    //         }
+    //     }
+    // }
+    // const handleCamera = async () => {
+    //     const constraints = {
+    //         audio: false,
+    //         video: { facingMode: "environment" }
+    //     };
 
-
-                }
-            };
-    }, [qrcode])
-    const tick = () => {
-        console.log('tick')
-        const canvas = canvasRef.current.getContext("2d");
-        console.log(canvas)
-        console.log(videoRef.current.videoHeight)
-        console.log(videoRef.current.videoWidth)
-        console.log(videoRef.current)
-        canvasRef.current.height = videoRef.current.videoHeight
-        canvasRef.current.width = videoRef.current.videoWidth
-        canvas.drawImage(videoRef.current, 0, 0, canvasRef.current.height, canvasRef.current.width);
-        scanning && requestAnimationFrame(tick);
-    }
-    const scan = useCallback(() => {
-        if (qrcode) {
-
-            try {
-                qrcode.decode()
-            } catch (e) {
-                setTimeout(scan, 300);
-            }
-        }
-    }, [qrcode])
-    const handleCamera = async () => {
-        const constraints = {
-            audio: false,
-            video: { facingMode: "environment" }
-        };
-
-        navigator.mediaDevices.getUserMedia(
-            constraints //will change
-        ).then(stream => {
-            videoRef.current.srcObject = stream;
-            canvasRef.current.hidden = false
-            setScanning(true)
-            tick()
-            scan()
-        })
-    }
-
+    //     navigator.mediaDevices.getUserMedia(
+    //         constraints //will change
+    //     ).then(stream => {
+    //         videoRef.current.srcObject = stream;
+    //         canvasRef.current.hidden = false
+    //         setScanning(true)
+    //         tick()
+    //         scan()
+    //     })
+    // }
+    // console.log(scanning)
     return (
         <div>
-            <div onClick={handleCamera}>
+            <div >
+                <QrReader
+                    onResult={(result, error) => {
+                        if (!!result) {
+                            setData(result?.text);
+                        }
+
+                        if (!!error) {
+                            console.info(error);
+                        }
+
+                    }
+                    }
+                    constraints={{ facingMode: "environment" }}
+                    style={{ width: "40%", height: "40%" }}
+                />
+                <p>{data}</p>
+            </div>
+            {/* <div onClick={handleCamera}>
                 camera on
             </div>
             <canvas ref={canvasRef} hidden={true} id="qr-canvas"></canvas>
@@ -86,9 +100,10 @@ const Qrscan = () => {
             <Script
                 src="https://rawgit.com/sitepoint-editors/jsqrcode/master/src/qr_packed.js"
                 onLoad={() => {
+                    console.log("set qr code")
                     setQrcode(window.qrcode)
                 }}
-            />
+            /> */}
         </div>
     );
 }
